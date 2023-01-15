9ed9d271e06e50f1f1c2338662fdf29afaeb0de8
Gisele <gisele.viedenhelfen@gmail.com>
2023-01-15 11:10:06 -0300

    adiciona volumes do frontend ao docker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+    <div className="App">   
+        <p
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+        React + Typescript
+        </p>
     </div>
   );
 }
